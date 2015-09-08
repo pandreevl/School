ea193318fe41ac055c6516e83c8c729ba287caae
Angel Chanquin <ajchanquin@gmail.com>
2015-09-08 10:13:56 -0600

    Init Project with connection to SQL with Entity Framework

---
+++ EntityDatabase/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports Modules.Departments.ViewModels
+Class MainWindow
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.DepartmenstUserControl.MainGrid.DataContext = New DepartmentsViewModel()
+
+    End Sub
+End Class
---
+++ EntityDatabase/Modules/Departments/Views/DepartmentsList.xaml.vb
@@ -0,0 +1,4 @@
+﻿Namespace Modules.Departments.Views
+    Public Class DepartmentsList
+    End Class
+End Namespace
