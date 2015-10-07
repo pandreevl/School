d46066c1922f9426da3bda130c33e4c2b2ecb92e
pandreevl <p.andreevl@hotmail.com>
2015-10-06 22:07:19 -0600

    CRUD create

---
+++ EntityDatabase/Modules/Persons/ViewModels/NewPerson.vb
@@ -0,0 +1,38 @@
+﻿Imports BusinessLogic.Helpers
+Imports BusinessLogic.Services.Implementations
+Imports BusinessLogic.Services.Interfaces
+Imports System.Collections
+Imports BusinessObjects.Helpers
+Imports Estructure.Command
+Imports Modules.Persons.ViewModel
+Namespace Modules.Persons.ViewModel
+    Public Class NewPerson
+        Inherits ViewModelBase
+        Public _Person As New Person
+        Public _aceptAdd As ICommand
+        Public _window As NewPersons
+
+        Public Property FirstName As String
+            Get
+                Return Me._Person.FirstName
+            End Get
+            Set(value As String)
+                Me._Person.FirstName = value
+                OnPropertyChanged("FirstName")
+            End Set
+
+        End Property
+
+        Public ReadOnly Property AceptedAdd As ICommand
+            Get
+                Return Me._aceptAdd
+
+            End Get
+        End Property
+
+        Sub New(ByRef newWindow As NewPersons)
+            Me._window = newWindow
+        End Sub
+
+    End Class
+End Namespace
--- EntityDatabase/Modules/Persons/ViewModels/PersonViewModel.vb
+++ EntityDatabase/Modules/Persons/ViewModels/PersonViewModel.vb
@@ -2,10 +2,13 @@
 Imports BusinessLogic.Services.Implementations
 Imports BusinessLogic.Services.Interfaces
 Imports System.Collections.ObjectModel
+Imports Estructure.Command
 Namespace Modules.Persons.ViewModels
     Public Class PersonViewModel
         Inherits ViewModelBase
+        Public Shadows person As NewPersons
         Private _person As ObservableCollection(Of Person)
+        Private _insertButtonCommand As ICommand
         Private dataAccess As IPersonService
 
         Public Property Persons As ObservableCollection(Of Person)
@@ -17,7 +20,39 @@ Namespace Modules.Persons.ViewModels
                 OnPropertyChanged("Persons")
             End Set
         End Property
+        Public Property InsertCommand As ICommand
+            Get
+                If Me._insertButtonCommand Is Nothing Then
+                    Me._insertButtonCommand = New Commands(AddressOf InsertPersonDB)
+                End If
+                Return Me._insertButtonCommand
+            End Get
+            Set(value As ICommand)
+                _insertButtonCommand = value
+            End Set
+        End Property
 
+        Sub InsertPersonDB()
+            Using context As New SchoolEntities
+                person = New NewPersons
+                Person.ShowDialog()
+
+                refresh()
+            End Using
+        End Sub
+
+        Sub refresh()
+            'Initialize property variable of departments
+            Me._person.Clear()
+            ' Register service with ServiceLocator
+            ServiceLocator.RegisterService(Of IPersonService)(New PersonService)
+            ' Initialize dataAccess from service
+            Me.dataAccess = GetService(Of IPersonService)()
+            ' Populate departments property variable 
+            For Each element In Me.GetAllPerson
+                Me._person.Add(element)
+            Next
+        End Sub
         ' Function to get all departments from service
         Private Function GetAllPerson() As IQueryable(Of Person)
             Return Me.dataAccess.GetAllPerson
